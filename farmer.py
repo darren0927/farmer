fc49108bf29b921e6eb870bfd0a361553bddfb7d
谈冬 <tandong.td@alibaba-inc.com>
2021-06-28 14:35:34 +0800

    set bs encode

--- farmer.py
+++ farmer.py
@@ -12,23 +12,18 @@ class Farmer(object):
         self.url = url
 
     def get_source_page(self):
-        """
-        返回经过lxml.html.fromstring 模块处理的<Element html at 0x36e8278>
-        可以用 XPath 来过滤数据
-        """
         try:
             response = urllib3.urlopen(self.url, timeout=3).read()
             selector = html.fromstring(response)
         except Exception as e:
-            print('获取网页内容报错 %s' % str(e))
+            print('An error occurred while fetching the content of the page: %s' % str(e))
             selector = None
         finally:
             return selector
 
     def work(self):
-        """ 获取妹子图片链接 """
         html_content = requests.get(self.url).text
-        soup = BeautifulSoup(html_content, 'lxml')
+        soup = BeautifulSoup(html_content, 'lxml', from_encoding="UTF-8")
         mm_img_content = soup.find_all('div', {"class": "list"})
         for ul in mm_img_content:
             mm_img_list = ul.find_all('img')
@@ -36,8 +31,8 @@ class Farmer(object):
                 url = img['src']
                 r = requests.get(url, stream=True)
                 image_name = url.split('/')[-1]
-                print('mm图片 %s' % url)
-                print('mm图片name %s' % image_name)
+                print('the href of image is %s' % url)
+                print('the name of image is %s' % image_name)
         return
 
 
